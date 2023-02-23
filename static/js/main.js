be1e9e8fd479e8397bbd583b4734fe8f56991021
Simon Rolly <simonrolly@outlook.com>
2023-02-23 23:51:16 +0100

    adds streak counter

--- static/js/main.js
+++ static/js/main.js
@@ -4,24 +4,53 @@ function func() {
     var int2 = document.getElementById("int2").innerHTML;
 
     if(answer != int1 * int2) {
-        document.getElementById("reply").innerHTML = "Wrong, try again.";
+        document.getElementById("reply").innerHTML = "Wrong, try again. ";
+
+        const text0 = "You're streak was ";
+        const streak = document.cookie.slice(7, );
+        let text1;
+        if(streak > 5) {
+            if(Math.floor(Math.random() * 2) == 0) {
+                text1 = ". Keep it up!";
+            }
+            else {
+                text1 = ". Good job!"
+            }
+            
+        }
+        else {
+            text1 = ". Keep practicing!";
+        }
+        document.getElementById("streak").innerHTML = text0.concat(streak).concat(text1);
+        document.cookie = "streak=0";
     }
     else {
         document.getElementById("reply").innerHTML = "Correct!";
-        
+        document.getElementById("streak").innerHTML = "";
+
+        // increase streak
+        const text = "streak=";
+        let streakCount = parseInt(document.cookie.slice(7, ));
+        if(Number.isNaN(streakCount)) {
+            streakCount = 0;
+        }
+        streakCount++;
+        document.cookie = text.concat(streakCount);
+
         let number1;
         let number2;
 
         const hardMode = document.getElementById('hardMode').checked; 
         if(hardMode) {
-            const maxBound = 13;
-            number1 = Math.floor(Math.random() * maxBound);
-            number2 = Math.floor(Math.random() * maxBound);
-            if(number1 == 0 || number1 == 1 || number1 == int1) { // new number is != 0 or 1 or equal to the previous number
-                number1 = Math.floor(Math.random() * maxBound);
+            const maxBound = 12;
+            const minBound = 2;
+            number1 = Math.floor(Math.random() * (maxBound - minBound + 1)) + minBound;
+            number2 = Math.floor(Math.random() * (maxBound - minBound + 1)) + minBound;
+            if(number1 == int1) { // new number is != 0 or 1 or equal to the previous number
+                number1 = Math.floor(Math.random() * (maxBound - minBound + 1)) + minBound;
             }
-            if(number2 == 0 || number2 == 1 || number2 == int2) { 
-                number2 = Math.floor(Math.random() * maxBound);
+            if(number2 == int2) { 
+                number2 = Math.floor(Math.random() * (maxBound - minBound + 1)) + minBound;
             }
         }
         else {
@@ -43,6 +72,9 @@ function func() {
             while(number2 == int2) {
                 number2 = Math.floor(Math.random() * maxBound)
             }
+            while(number1 == 0 && number2 == number1) {
+                number1 = Math.floor(Math.random() * 10) + 1;
+            }
         }
         
         document.getElementById("int1").innerHTML = number1;
@@ -52,15 +84,19 @@ function func() {
 }
 
 function pageLoad() {
-    // console.log("page load");
     document.getElementById("int1").innerHTML = Math.floor(Math.random() * 11);
     document.getElementById("int2").innerHTML = Math.floor(Math.random() * 11);
+
+    // check if streak cookie exists
+    if(document.cookie == "") {
+        document.cookie = "streak=0";
+    }
+
     darkMode();
 }
 
 function darkMode() {
     const darkMode = document.getElementById("darkMode").checked;
-    console.log(darkMode)
     if(darkMode) {
         document.body.style.backgroundColor = "#121e30";
         document.body.style.color = "#f7f8fa";
